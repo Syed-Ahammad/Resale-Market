5f204d1c2c77adeb0287742869413c7e0ed057a2
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-25 08:48:35 +0600

    SignUp Page Added

--- src/Component/Pages/SignUp/SignUp.js
+++ src/Component/Pages/SignUp/SignUp.js
@@ -1,8 +1,72 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+    const { register, handleSubmit, formState: { errors }} = useForm();
     return (
         <div>
+            <div className="w-96">
+        <h2 className="text-4xl text-center pb-6">Register here!! </h2>
+        <form onSubmit={handleSubmit()}>
+          <div className="form-control w-full max-w-sm">
+            <label className="label">
+              <span className="label-text font-semibold">Name</span>
+            </label>
+            <input
+              {...register("name")}
+              type="text"
+              className="input input-bordered w-full max-w-sm"
+            />
+          </div>
+          <div className="form-control w-full max-w-sm">
+            <label className="label">
+              <span className="label-text font-semibold">Email</span>
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              className="input input-bordered w-full max-w-sm"
+            />
+          </div>
+          <div className="form-control w-full max-w-sm">
+            <label className="label">
+              <span className="label-text font-semibold">Password</span>
+            </label>
+            <input
+              {...register("password", {
+                required: "Password is required",
+                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$*])(?=.*[0-9])/ },
+                message: "password must be strong",
+              })}
+              type="password"
+              className="input input-bordered w-full max-w-sm"
+              aria-invalid={errors.password ? "true" : "false"}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password?.message}</p>
+            )}
+          </div>
+          <label className="label">
+            <span className="label-text font-semibold text-red">
+              {/* {errorMess} */}
+            </span>
+          </label>
+          <input type="submit" className="btn btn-active w-full mt-6" />
+          <label className="label">
+            <span className="label-text-alt text-[16px] text-center">
+              Already have an account?
+              <Link className="text-[#19D3AE] font-normal" to={"/login"}>
+                Please Log In.
+              </Link>
+            </span>
+          </label>
+          <div className="divider">OR</div>
+          <button className="btn btn-outline w-full">
+            Continue With Google
+          </button>
+        </form>
+      </div>
             
         </div>
     );
--- src/Component/SharedPage/Navbar/Navbar.js
+++ src/Component/SharedPage/Navbar/Navbar.js
@@ -2,6 +2,34 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
+    const menuItems = (
+        <React.Fragment>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          
+            <li>
+              <Link onClick={''}>Sign Out</Link>
+            </li>
+        
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+         
+        </React.Fragment>
+      );
   return (
     <div className="navbar bg-primary text-white">
       <div className="navbar-start">
@@ -24,70 +52,17 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52"
           >
-            <li>
-              <Link>Item 1</Link>
-            </li>
-            <li tabIndex={0}>
-              <Link className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </Link>
-              <ul className="p-2">
-                <li>
-                  <Link>Submenu 1</Link>
-                </li>
-                <li>
-                  <Link>Submenu 2</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link>Item 3</Link>
-            </li>
+            {menuItems}
+
           </ul>
         </div>
         <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link>Item 1</Link>
-          </li>
-          <li tabIndex={0}>
-            <Link>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul className="p-2">
-              <li>
-                <Link>Submenu 1</Link>
-              </li>
-              <li>
-                <Link>Submenu 2</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link>Item 3</Link>
-          </li>
+        {menuItems}
         </ul>
       </div>
       <div className="navbar-end">
