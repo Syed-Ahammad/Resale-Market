d5ce68036ad4b92c28044e3264caaca32435d64b
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-25 18:50:02 +0600

    firebase authentication setup

---
+++ src/FirebaseAuth/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdE6LQEUfJZS_5O2pleTCajmT6pqHxyi0",
+  authDomain: "resale-market-562ef.firebaseapp.com",
+  projectId: "resale-market-562ef",
+  storageBucket: "resale-market-562ef.appspot.com",
+  messagingSenderId: "733692709480",
+  appId: "1:733692709480:web:a7377c2aba2fbe46df09dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
